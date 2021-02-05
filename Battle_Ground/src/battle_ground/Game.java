4aa44a0a3d1c4157e5b7efe13f20e99888a49826
WuZedon.Tom <zedong0901@outlook.com>
2021-02-04 23:19:19 -0800

    added Screen class and timer

--- Battle_Ground/src/battle_ground/Game.java
+++ Battle_Ground/src/battle_ground/Game.java
@@ -9,17 +9,21 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import battle_ground.graphics.Screen;
+
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 	
 	// Set the screen size and scale of the game;
-	public static int width = 400;
+	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 	
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
+	
+	private Screen screen;
 	
 	private BufferedImage image = new BufferedImage(width, height,
 								BufferedImage.TYPE_INT_RGB);
@@ -30,6 +34,8 @@ public class Game extends Canvas implements Runnable {
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size); // Canvas method;
+		
+		screen = new Screen(width, height);
 		
 		frame = new JFrame();
 	}
@@ -49,16 +55,28 @@ public class Game extends Canvas implements Runnable {
 		}
 	}
 	
-	// Design the game loop;
 	@Override
 	public void run() {
+		long lastTime = System.nanoTime();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
+		
 		while (running) {
-			update(); // handles game logics
-			render(); // does rendering
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			
+			while (delta >= 1) {
+				update();
+				delta --;
+			}
+			render();
 		}
+		stop();
 	}
 	
-	public void update() {	
+	public void update() {
+		
 	}
 	
 	public void render() {
@@ -67,14 +85,20 @@ public class Game extends Canvas implements Runnable {
 			createBufferStrategy(3);
 			return;
 		}
+		screen.clear();
+		screen.render();
+		
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
 		
 		Graphics g = bs.getDrawGraphics();
 		g.setColor(Color.BLACK);
 		
-		// fill a rectangle starting from (0, 0) top-left
 		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
-		bs.show(); // make the buffer visible
+		bs.show(); 
 	}
 	
 	public static void main(String[] args) {
@@ -84,12 +108,10 @@ public class Game extends Canvas implements Runnable {
 		game.frame.add(game);
 		game.frame.pack();
 		
-		// close the process (game) when click the 'x' button;
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		game.frame.setLocationRelativeTo(null);
 		
-		// show the window;
 		game.frame.setVisible(true);
 		
 		game.start();
---
+++ Battle_Ground/src/battle_ground/graphics/Screen.java
@@ -0,0 +1,39 @@
+package battle_ground.graphics;
+
+public class Screen {
+	
+	private int width, height;
+	public int[] pixels;
+	
+	int xtime = 100,ytime = 50;
+	
+	int time = 0;
+	int counter = 0;
+	
+	public Screen(int width, int height) {
+		this.width = width;
+		this.height = height;
+		pixels = new int[width * height];
+	}
+	
+	public void clear() {
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0;
+		}
+	}
+	
+	public void render() {
+		counter ++;
+		if (counter % 100 == 0) xtime++;
+		if (counter % 800 == 0) ytime++;
+		
+		for (int y = 0; y < height; y++) {
+			if (ytime < 0 || ytime >= height) break;
+			
+			for (int x = 0; x < width; x++) {
+				if (xtime < 0 || xtime >= width) break;
+				pixels[xtime + ytime * width] = 0xff00ff;
+			}
+		}
+	}
+}
